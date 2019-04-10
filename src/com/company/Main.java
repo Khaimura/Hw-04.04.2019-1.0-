d5d9871facc840a3fa3b1f014d9947e7ff6976ad
Bohdan <48527847+Khaimura@users.noreply.github.com>
2019-04-10 21:37:49 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,19 +2,14 @@ package com.company;
 
 public class Main {
 
-
-
     public static void main(String[] args) {
-   // Cat catOne = new Cat();
-    Cat catOne = new Cat("Adolf","M",4,"Polydactyl","Black");
-        catOne.setNewYears(catOne,2);
-   // Cat catTwo = new Cat("Shani","F",4,"Unknown","White and Black");
-
-
-    catOne.print();
-    catOne.theVoiceOfTheCat();
-    catOne.slepp();
-    //catTwo.print();
-    //catTwo.slepp();
+        Cat catOne = new Cat("Adolf", "M", 4, "Polydactyl", "Black");
+        catOne.setNewYears(catOne, 2);
+        System.out.println(catOne.toString());
+        catOne.theVoiceOfTheCat();
+        catOne.slepp();
+        // Cat catTwo = new Cat("Shani","F",4,"Unknown","White and Black");
+        //catTwo.print();
+        //catTwo.slepp();
     }
 }
